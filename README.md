45c2d540c392f281a491ecee5a322a564220d1cc
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2016-01-26 09:47:29 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,126 @@
+
+>  HTML Sitemap plugin for Assemble
+
+Register the plugin with Assemble:
+
+```js
+assemble: {
+  options: {
+    plugins: ['grunt-assemble-html-sitemap', 'other/plugins/*']
+  }
+}
+```
+
+Visit the [plugins docs](http://assemble.io/plugins/) for more info or for help getting started.
+
+
+## dest
+
+Type: `String`
+
+Default: `undefined`
+
+Sitemap destination. If not set, fallback to assemble destination.
+
+## homepage
+
+Type: `String`
+
+Default: `homepage` (from package.json)
+
+Site URL
+
+## exclusions
+
+Type: `Array`
+
+Default: `['404']`
+
+Pages to omit from the sitemap.
+
+```js
+options: {
+  sitemap: {
+    exclusions: ["foo", "bar"],
+  },
+  files: {
+    ...
+  }
+}
+```
+
+## relativedest
+
+Type: `String` / `Boolean`
+
+Default: `false`
+
+Path to which the URLs in Sitemap and Robots should be relative to. `true` is equal to the destination path `dest` and `false` is equal to the root directory.
+
+## Usage Examples
+
+## Simple
+
+To simplify might do something like:
+
+```js
+assemble: {
+  blog: {
+    options: {
+      plugins: ['grunt-assemble-html-sitemap'],
+    },
+    files: {
+      './blog/': ['./templates/blog/*.hbs']
+    }
+  }
+}
+```
+
+## Result
+
+```js
+./blog/sitemap.html
+```
+
+## Advanced
+
+```js
+assemble: {
+  blog: {
+    options: {
+      plugins: ['grunt-assemble-sitemap'],
+      sitemap: {
+        homepage: 'http://assemble.io',
+            exclude: ['50x', 'foo']
+      }
+    },
+    files: {
+      './blog/': ['./templates/blog/*.hbs']
+    }
+  }
+}
+```
+
+## Result
+
+```js
+./blog/sitemap.html
+```
+
+
+## Author
+
+**Ashley Hitchcock**
+
++ [github/ashhitch](https://github.com/ashhitch)
++ [twitter/ash_hitchcock](http://twitter.com/ash_hitchcock)
++ [ashleyhitchcock.co.uk](http:/www.ashleyhitchcock.co.uk)
+
+## License
+
+Copyright © 2016 Ashley Hitchcock
+Released under the MIT license.
+
+## Credits
+Based on the XML plugin:
++ [grunt-assemble-sitemap] https://github.com/assemble/grunt-assemble-sitemap
---
+++ index.js
@@ -0,0 +1,90 @@
+/*!
+ * grunt-assemble-sitemap <https://github.com/ashhitch/grunt-assemble-html-sitemap.git>
+ *
+ * Copyright (c) 2016, Ashley Hitchcock.
+ * Licensed under the MIT License.
+ */
+
+var jstohtml =  require('jstohtml');
+var async = require('async');
+var _ = require('lodash');
+var path = require('path');
+
+module.exports  = function (params, callback) {
+
+  var assemble  = params.assemble;
+  var grunt     = params.grunt;
+  var pages     = assemble.options.pages;
+  var options   = assemble.options.htmlSitemap || {};
+  var sitemap   = [];
+  var exclusion = ['404', '500', '403'];
+  var pkg       = grunt.file.readJSON('package.json');
+
+  options.homepage = options.homepage || pkg.homepage;
+  options.dest = options.dest || path.dirname(pages[0].dest);
+
+  // Only write if it actually changed.
+  var write = function (file, content) {
+    var msg;
+    var old = grunt.file.exists(file) ? grunt.file.read(file) : '';
+
+    if (old !== content) {
+      grunt.file.write(file, content);
+      msg = 'Created '.yellow + file.cyan;
+    } else {
+      msg = 'Keeping '.yellow + file.cyan;
+    }
+    return grunt.verbose.ok(msg);
+  };
+
+  // Return the relative destination if the option is enabled
+  var getExternalFilePath = function (relativedest, file) {
+    if(relativedest === true) {
+      relativedest = options.dest;
+    }
+    return (relativedest ? file.dest.replace(relativedest + "/", "") : file.dest );
+  };
+
+  var url = options.homepage || '';
+  var relativedest = options.relativedest;
+
+  async.forEach(pages, function (file, next) {
+
+    if (!_.isUndefined(options.exclude)) {
+      exclusion = _.union([], exclusion, options.exclude || []);
+    }
+
+//push to array
+    sitemap.push({
+        t: 'li',
+        c: [{
+          t: 'a',
+          href: url + '\\' + getExternalFilePath(relativedest, file),
+          c: getExternalFilePath(relativedest, file)
+        }]
+    });
+
+    next();
+  }, callback());
+
+//build HTML
+ var result = jstohtml([
+    {
+        t:'h1',
+        c: 'Sitemap:'
+    },
+    {
+        t: 'ul',
+        cl: ['sitemap'],
+        c: [sitemap]
+    }
+]);
+
+  var sitemapDest = options.dest + '/sitemap.html';
+  write(sitemapDest, result);
+
+};
+
+module.exports.options = {
+  stage: 'render:pre:pages'
+};
